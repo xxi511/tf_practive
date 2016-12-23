548fd1eb8894923813a26b637c1c07205cd6dd18
xxi511 <s941314g@yahoo.com.tw>
2016-12-23 22:45:15 +0800

    mnist

---
+++ mnist/mine_mnist_conv.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+import argparse
+import sys
+
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+
+'''
+2 conv + 2 fc
+
+'''
+
+def weight_init(shape):
+    # shape = [h, w, input_chanel, num_kernel]
+    init = tf.truncated_normal(shape, stddev=0.1)
+    return tf.Variable(init)
+
+
+def bias_init(shape):
+    init = tf.constant(0.1, shape=shape)
+    return tf.Variable(init)
+
+
+def conv2d(x, w):
+    # strides = [batch, h, w, chanel]
+    # the filter won't cross batch or chanel
+    return tf.nn.conv2d(x, w, strides=[1, 1, 1, 1], padding='SAME')
+
+
+def max_pool_2x2(x):
+    # ksize: kernel size
+    # [batch, h, w, chanel], create a filter just cover h and w
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1],
+                          padding='SAME')
+
+
+def main(_):
+    mnist = input_data.read_data_sets(FLAGS.data_dir, one_hot=True)
+    x = tf.placeholder(tf.float32, [None, 784])
+    y_ = tf.placeholder(tf.float32, [None, 10])
+    keep_prob = tf.placeholder(tf.float32)
+
+    x_image = tf.reshape(x, [-1, 28, 28, 1])
+
+    w_conv1 = weight_init([5, 5, 1, 32])
+    b_conv1 = bias_init([32])
+    res_conv1 = tf.nn.relu(conv2d(x_image, w_conv1) + b_conv1)
+    res_pool1 = max_pool_2x2(res_conv1)
+
+    w_conv2 = weight_init([5, 5, 32, 64])
+    b_conv2 = bias_init([64])
+    res_conv2 = tf.nn.relu(conv2d(res_pool1, w_conv2) + b_conv2)
+    res_pool2 = max_pool_2x2(res_conv2)
+    flat_pool2 = tf.reshape(res_pool2, [-1, 7*7*64])
+
+    w_fc3 = weight_init([7*7*64, 1024])
+    b_fc3 = bias_init([1024])
+    res_fc3 = tf.nn.relu(tf.matmul(flat_pool2, w_fc3) + b_fc3)
+    res_fc3 = tf.nn.dropout(res_fc3, keep_prob)
+
+    w_fc4 = weight_init([1024, 10])
+    b_fc4 = bias_init([10])
+    y = tf.matmul(res_fc3, w_fc4) + b_fc4
+
+    loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y, y_))
+    train = tf.train.AdamOptimizer(1e-4).minimize(loss)
+
+    correct_predict = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_predict, tf.float32))
+
+    var_init = tf.initialize_all_variables()
+
+    with tf.Session() as sess:
+        sess.run(var_init)
+
+        for i in xrange(20000):
+            batch = mnist.train.next_batch(50)
+            if i % 100 == 0:
+                train_accuracy = accuracy.eval(feed_dict={
+                    x: batch[0], y_: batch[1], keep_prob: 1.0})
+                print("step %d, training accuracy %g" % (i, train_accuracy))
+            sess.run(train, feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+
+        print("test accuracy %g" % accuracy.eval(feed_dict={
+            x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_dir', type=str, default='/tmp/tensorflow/mnist/input_data',
+                        help='Directory for storing input data')
+    FLAGS, unparsed = parser.parse_known_args()
+    tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
--- mnist/mine_mnist_softmax.py
+++ mnist/mine_mnist_softmax.py
@@ -6,33 +6,24 @@ import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 
 '''
-two fully connected layer
-use dropout and relu
-accuracy 0.9641
+just one fully connected layer
+accuracy 0.919
 '''
+
 def mlp(_):
     # Import data
     mnist = input_data.read_data_sets(FLAGS.data_dir, one_hot=True)
 
     x = tf.placeholder(tf.float32, [None, 784])
     y_ = tf.placeholder(tf.float32, [None, 10])
-    keep_prob = tf.placeholder(tf.float32)
 
-    w = tf.Variable(tf.truncated_normal([784, 625], stddev=0.05))
-    b = tf.Variable(tf.truncated_normal([625], stddev=0.05))
-    y1 = tf.add(tf.matmul(x, w), b)
-    y1 = tf.nn.relu(y1)
-    y1 = tf.nn.dropout(y1, keep_prob)
-
-    w2 = tf.Variable(tf.zeros([625, 10]))
-    b2 = tf.Variable(tf.zeros([10]))
-    y = tf.add(tf.matmul(y1, w2), b2)
-    # y2 = tf.nn.relu(y2)
-    # y2 = tf.nn.dropout(y2, keep_prob)
-    #
-    # w3 = tf.Variable(tf.zeros([400, 10]))
-    # b3 = tf.Variable(tf.zeros([10]))
-    # y = tf.add(tf.matmul(y2, w3), b3)
+    # accuracy is 0.91
+    w = tf.Variable(tf.truncated_normal([784, 10], stddev=0.05))
+    b = tf.Variable(tf.truncated_normal([10], stddev=0.05))
+    # accuracy is 0.88, WT
+    # w = tf.Variable(tf.zeros([784, 10]))
+    # b = tf.Variable(tf.zeros([10]))
+    y = tf.add(tf.matmul(x, w), b)
 
     loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y, y_))
     train = tf.train.GradientDescentOptimizer(0.5).minimize(loss)
@@ -43,14 +34,15 @@ def mlp(_):
 
         for i in xrange(1000):
             x_info, y_info = mnist.train.next_batch(100)
-            sess.run(train, feed_dict={x: x_info, y_: y_info, keep_prob: 0.5})
+            if i % 100 == 0:
+                print 'b is {}'.format(b.eval())
+            sess.run(train, feed_dict={x: x_info, y_: y_info})
 
         # Test trained model
         correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
         accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
         print(sess.run(accuracy, feed_dict={x: mnist.test.images,
-                                            y_: mnist.test.labels,
-                                            keep_prob: 1}))
+                                            y_: mnist.test.labels}))
 
 
 if __name__ == '__main__':
